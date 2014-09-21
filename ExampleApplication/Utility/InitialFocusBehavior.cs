e4e96292090746b702668c10c9eaa78fc9b7cc29
Paul Spangler <github@spanglerco.com>
2014-09-21 13:55:00 -0500

    Adding license to source files
    Applying a header to each source file pointing to the license.

--- ExampleApplication/Utility/InitialFocusBehavior.cs
+++ ExampleApplication/Utility/InitialFocusBehavior.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿// Copyright © 2014 Paul Spangler
+//
+// Licensed under the MIT License (the "License");
+// you may not use this file except in compliance with the License.
+// You should have received a copy of the License with this software.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Windows;
 using System.Windows.Threading;
 
